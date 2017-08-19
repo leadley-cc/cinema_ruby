266323215d18972219ff18ae2a93ac82d3cbf7d8
Michael Leadley <leadley75@gmail.com>
2017-08-19 16:24:07 +0100

    Update Ticket model with screening_id

--- models/ticket.rb
+++ models/ticket.rb
@@ -2,12 +2,13 @@ require_relative "../db/sql_runner"
 
 class Ticket
   attr_reader :id
-  attr_accessor :customer_id, :film_id
+  attr_accessor :customer_id, :film_id, :screening_id
 
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @customer_id = options["customer_id"].to_i
     @film_id = options["film_id"].to_i
+    @screening_id = options["screening_id"].to_i
   end
 
   def customer
@@ -24,11 +25,11 @@ class Ticket
 
   def save
     sql = "
-      INSERT INTO tickets (customer_id, film_id)
-      VALUES ($1, $2)
+      INSERT INTO tickets (customer_id, film_id, screening_id)
+      VALUES ($1, $2, $3)
       RETURNING id
     "
-    values = [@customer_id, @film_id]
+    values = [@customer_id, @film_id, @screening_id]
     result = SqlRunner.run(sql, values)
     @id = result[0]["id"].to_i
   end
@@ -36,10 +37,10 @@ class Ticket
   def update
     sql = "
       UPDATE tickets
-      SET (customer_id, film_id) = ($1, $2)
+      SET (customer_id, film_id, screening_id) = ($1, $2, $3)
       WHERE id = $3
     "
-    values = [@customer_id, @film_id, @id]
+    values = [@customer_id, @film_id, @screening_id, @id]
     SqlRunner.run(sql, values)
   end
 
