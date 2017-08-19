900ed18c98cc54a1008b13c9c1c78926308eefb0
Michael Leadley <leadley75@gmail.com>
2017-08-19 16:55:18 +0100

    Correction to prvious commit

--- db/codeclan_cinema.sql
+++ db/codeclan_cinema.sql
@@ -18,7 +18,7 @@ CREATE TABLE films (
 CREATE TABLE screenings (
   id SERIAL PRIMARY KEY,
   film_id INT REFERENCES films(id) NOT NULL,
-  timedate TIMESTAMP NOT NULL
+  date_time TIMESTAMP NOT NULL
 );
 
 CREATE TABLE tickets (
